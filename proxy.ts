2f95a9bae0b6bea91607cdf3740214b58d20af0b
somonster275 <somonster275@gmail.com>
2026-06-27 15:46:31 +0200

    Exclure icon.svg du proxy pour servir le favicon (etait redirige vers login)

--- proxy.ts
+++ proxy.ts
@@ -50,5 +50,5 @@ export async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|icon.svg|apple-icon).*)'],
 }
